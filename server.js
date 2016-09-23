84590d202a3dc7d3a81e5ddc8f26d3f720b8d610
ChandrasekarThiyagarajan <chandrasekar.thiyagarajan@prosense.co.in>
2016-09-24 03:12:44 +0700

    updated script-reassure

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/:articleName', function (req, res) {
-  var articleName= req.params.articleName
+  var articleName= req.params.articleName;
   res.send(creatTemplate(articles[articleName]));
 });
 
